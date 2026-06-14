71b5572362c908b6dbe530fd24a7f688c48adb6f
Corey Ward <corey@hey.com>
2026-06-14 17:47:22 -0500

    Apply formatter

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
 const rootElement = document.getElementById("root");
-if (!rootElement) {throw new Error("Root element not found");}
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
 
 createRoot(rootElement).render(
   <StrictMode>
